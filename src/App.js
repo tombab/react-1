62adc6498f66ca2b686302abbb2c696fd0c491a6
Thomas Baronnet <69805103+tombab@users.noreply.github.com>
2022-06-02 18:53:43 +0200

    feat: added state management and checkboxes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,58 @@
+import React, { useState } from "react";
+import GenericCheckbox from "./components/generic-checkbox";
+import "./App.css";
 
-import './App.css';
+const App = () => {
+  // ================================================= ARRAYS
+  const arrayToFill = new Array(5);
 
-const App = () =>{
+  // ================================================= STATE
+  const [isChecked, setIsChecked] = useState(arrayToFill.fill(false));
+
+  // ================================================= INLINE METHODS
+  const toggleCheck = (index) => {
+    setIsChecked((prevState) => {
+      const newState = [...prevState];
+      newState[index] = !prevState[index];
+      return newState;
+    });
+  };
+
+  const toggleCheckAll = () => {
+    if (isChecked.includes(false)) {
+      setIsChecked([...arrayToFill].fill(true));
+    } else {
+      setIsChecked([...arrayToFill].fill(false));
+    }
+  };
+
   return (
     <div className="app">
-      Init
+      <div className="checkboxContainer">
+        <input
+          type="checkbox"
+          id="selectAll"
+          checked={isChecked.includes(false) ? false : true}
+          onChange={toggleCheckAll}
+        />
+        <label htmlFor="selectAll">Select all</label>
+
+        {isChecked.map((check, index) => {
+          return (
+            <GenericCheckbox
+              key={index}
+              id={index}
+              label={index+1}
+              onChange={() => {
+                toggleCheck(index);
+              }}
+              checked={check}
+            />
+          );
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
